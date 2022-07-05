2770beecd5d065cbc447f2f9897547a1dea3014d
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-07-04 23:28:13 -0300

    Upgrade layout

--- App.js
+++ App.js
@@ -1,25 +1,64 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Lua from './assets/lua.png'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.palavra}>helloo</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <Image style={estilos.img} source={Lua} />
+      <View style={estilos.boxTexto}>
+        <Text style={estilos.titulo}>Lua</Text>
+
+        <Text style={estilos.texto}>
+          A Lua é o satélite natural do{' '}
+          <Text style={estilos.destaque}>planeta Terra</Text>, distanciados por
+          aproximadamente 384.405 km.
+        </Text>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center'
   },
 
-  palavra: {
-    marginTop: 100,
-    fontSize: 30
+  img: {
+    width: 200,
+    height: 200,
+    marginBottom: 10
+  },
+
+  boxTexto: {
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 20,
+    borderStyle: 'dotted',
+    width: 300,
+    paddingVertical: 10,
+    paddingHorizontal: 25
+  },
+
+  titulo: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+
+  texto: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#fff'
+  },
+  destaque: {
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: '#fff'
   }
 })
